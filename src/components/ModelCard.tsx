80ebdc83e6ac5f6cd97a5fe513431614ac27883f
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2025-12-07 13:48:42 +0200

    Format model stats in millions

--- src/components/ModelCard.tsx
+++ src/components/ModelCard.tsx
@@ -21,6 +21,9 @@ interface ModelCardProps {
 }
 
 const formatNumber = (num: number): string => {
+  if (num >= 1_000_000) {
+    return (num / 1_000_000).toFixed(1) + 'M';
+  }
   if (num >= 1000) {
     return (num / 1000).toFixed(1) + 'k';
   }
