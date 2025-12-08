66ddb5dcd84ae036c86cc552624693d476d6028d
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2025-12-08 14:28:33 +0200

    Update Amara's image path in Models.tsx

--- src/pages/Models.tsx
+++ src/pages/Models.tsx
@@ -31,7 +31,7 @@ const initialModels: Model[] = [
     name: 'Amara',
     niche: 'Fitness',
     subtitle: 'Athletic elegance meets lifestyle storytelling',
-    image: '/amara/2.jpg',
+    image: '/amara/11.jpg',
     likes: 98500,
     brandUses: 68,
     comments: 1300,
