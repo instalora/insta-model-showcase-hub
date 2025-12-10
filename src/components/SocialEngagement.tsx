17caded91a0b23110518cfeb8cf8dc6382efcc6a
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2025-12-10 20:09:37 +0200

    Update SocialEngagement.tsx

--- src/components/SocialEngagement.tsx
+++ src/components/SocialEngagement.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { UserRound } from 'lucide-react';
+import { UserRound, ThumbsUp } from 'lucide-react';
 
 
 interface SocialEngagementProps {
@@ -46,9 +46,9 @@ const SocialEngagement: React.FC<SocialEngagementProps> = ({
       </div>
       
       <div className="social-stats">
-        <svg width="18" height="18" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M18 8H17V6C17 3.24 14.76 1 12 1C9.24 1 7 3.24 7 6V8H6C4.9 8 4 8.9 4 10V20C4 21.1 4.9 22 6 22H18C19.1 22 20 21.1 20 20V10C20 8.9 19.1 8 18 8ZM12 17C10.9 17 10 16.1 10 15C10 13.9 10.9 13 12 13C13.1 13 14 13.9 14 15C14 16.1 13.1 17 12 17ZM15 8H9V6C9 4.34 10.34 3 12 3C13.66 3 15 4.34 15 6V8Z" fill="currentColor" />
-        </svg>
+        <ThumbsUp
+          className={`w-5 h-5 transition-colors text-muted-foreground`}
+        />
         <span>{formatNumber(shares)}</span>
       </div>
       
