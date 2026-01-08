f5063fbf9436163d643b585fa57de272d15db686
Kuprovskiy <oleg.kuprovskiy@gmail.com>
2026-01-08 14:50:06 +0200

    Update links.ts

--- src/constants/links.ts
+++ src/constants/links.ts
@@ -1,4 +1,4 @@
 export const START_FREE_TRIAL_URL =
   "https://app.epictwin.co/login?utm_source=website&utm_medium=cta&utm_campaign=start_free_trial";
 export const LOGIN_URL = "https://app.epictwin.co/login";
-export const SIGNUP_URL = "https://app.epictwin.co/signup";
+export const SIGNUP_URL = "https://app.epictwin.co/login";
